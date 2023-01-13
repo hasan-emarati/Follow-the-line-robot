c54be940cec0f792ab0911831ff1e598defeda06
SHemarati <hasan.emarati.12652@gmail.com>
2023-01-13 23:48:32 +0330

    last virsion

---
+++ My-code/code.c
@@ -0,0 +1,126 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+? Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 13/01/2023
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*******************************************************/
+
+#include <mega16.h>
+#include <delay.h>
+
+// Declare your global variables here
+
+void main(void)
+{
+    // Declare your local variables here
+    
+    // Input/Output Ports initialization
+    // Port A initialization
+    // Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+    DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+    // State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+    PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+    
+    // Port B initialization
+    // Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=Out Bit2=In Bit1=In Bit0=In 
+    DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (1<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+    // State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=0 Bit2=T Bit1=T Bit0=T 
+    PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+    
+    // Port C initialization
+    // Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=Out Bit2=Out Bit1=Out Bit0=Out 
+    DDRC=(1<<DDC7) | (1<<DDC6) | (1<<DDC5) | (1<<DDC4) | (1<<DDC3) | (1<<DDC2) | (1<<DDC1) | (1<<DDC0);
+    // State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=0 Bit2=0 Bit1=0 Bit0=0 
+    PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+    
+    // Port D initialization
+    // Function: Bit7=Out Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+    DDRD=(1<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+    // State: Bit7=0 Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+    PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+    
+    // Timer/Counter 0 initialization
+    // Clock source: System Clock
+    // Clock value: 8000.000 kHz
+    // Mode: Fast PWM top=0xFF
+    // OC0 output: Non-Inverted PWM
+    // Timer Period: 0.032 ms
+    // Output Pulse(s):
+    // OC0 Period: 0.032 ms Width: 0 us
+    TCCR0=(1<<WGM00) | (1<<COM01) | (0<<COM00) | (1<<WGM01) | (0<<CS02) | (0<<CS01) | (1<<CS00);
+    TCNT0=0x00;
+    OCR0=0x00;
+    
+    
+    // Timer/Counter 2 initialization
+    // Clock source: System Clock
+    // Clock value: 8000.000 kHz
+    // Mode: Fast PWM top=0xFF
+    // OC2 output: Non-Inverted PWM
+    // Timer Period: 0.032 ms
+    // Output Pulse(s):
+    // OC2 Period: 0.032 ms Width: 0 us
+    ASSR=0<<AS2;
+    TCCR2=(1<<PWM2) | (1<<COM21) | (0<<COM20) | (1<<CTC2) | (0<<CS22) | (0<<CS21) | (1<<CS20);
+    TCNT2=0x00;
+    OCR2=0x00;
+
+    
+    
+    
+    
+    // Analog Comparator initialization
+    // Analog Comparator: Off
+    // The Analog Comparator's positive input is
+    // connected to the AIN0 pin
+    // The Analog Comparator's negative input is
+    // connected to the AIN1 pin
+    ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+    SFIOR=(0<<ACME);
+    
+    
+    
+    
+    
+    while (1)
+    {
+        if (PINA==0b0001000)
+        {
+            PORTC.0=1;
+            OCR0=255;
+            OCR2=255;
+            PORTC.1=0;    
+            OCR0=255;
+            OCR2=255;   
+            PORTC.2=1;
+            OCR0=255;
+            OCR2=255;
+            PORTC.3=0;    
+            OCR0=255;
+            OCR2=255;
+        }
+        else
+        {
+            PORTC=-0X00;
+            OCR0=255;
+            OCR2=255;
+        }
+        
+    }
+}
